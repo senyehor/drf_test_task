36d116fdfa361ce7c52bf49e0eb7d199ea2c6f58
Yehor Sencha <senyehor@gmail.com>
2023-10-19 11:51:12 +0300

    feat: create person and group models

--- people_and_groups/models.py
+++ people_and_groups/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+        default=None
+    )
+    surname = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+        default=None
+    )
+    email = models.EmailField(
+        null=False,
+        blank=False,
+        unique=True,
+        default=None
+    )
+
+
+class Group(models.Model):
+    name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+    members = models.ManyToManyField(
+        Person,
+        related_name='groups'
+    )
