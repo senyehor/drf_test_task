b97a716c29ec646e60ce3a61f6d96ac9d9238e6e
Yehor Sencha <senyehor@gmail.com>
2023-10-22 13:39:46 +0300

    feat: add get person group list test

--- people_and_groups/tests/api/test_person_group_actions.py
+++ people_and_groups/tests/api/test_person_group_actions.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from people_and_groups.logic.exceptions import PersonAlreadyInThisGroup, PersonNotInThisGroup
 from people_and_groups.tests.factories import GroupFactory, PersonFactory
 from utils.tests.drf.utils import create_admin_client
+from utils.tests.utils import \
+    ensure_expected_model_objects_ids_match_serialized_objects_ids_and_count
 
 
 class TestPersonGroupActions(TestCase):
@@ -14,6 +16,22 @@ class TestPersonGroupActions(TestCase):
         self.__group = GroupFactory()
         self.__admin_client = create_admin_client()
         super().setUp()
+
+    def test_get_groups(self):
+        groups = [GroupFactory() for _ in range(5)]
+        for group in groups:
+            group.members.add(self.__person)
+        response = self.__admin_client.get(
+            reverse('person-groups', kwargs={'pk': self.__person.id})
+        )
+        self.assertEquals(
+            response.status_code, status.HTTP_200_OK,
+            'invalid response code'
+        )
+        ensure_expected_model_objects_ids_match_serialized_objects_ids_and_count(
+            groups,
+            response.json()['groups']
+        )
 
     def test_add_group(self):
         response = self.__admin_client.post(
